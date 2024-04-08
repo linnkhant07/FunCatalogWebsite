88852492b8671c011989c9dee0eafe89807b34ca
linnkhant07 <lkty07@gmail.com>
2024-04-07 17:50:04 -0700

    shallow copy allPokemons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,41 +1,21 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
+/* Linn Khant Thuya: Data Catalog Project Starter Code - SEA Stage 2 */
 
-
-//array of objects to hold pokemons
+//array of objects that  holds 100 pokemons
 import allPokemons from "./pokemons.js";
 
 //this array stores pokemons to display
-let displayPokemons = allPokemons
+let displayPokemons = [...allPokemons] //shallow copy
+
+//for pokemon sprites
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 const baseURL_back = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/'
 const baseURL_sound = "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/"
+
+//helper variables
 let sortHighToLow = false
 let pageChanged = false
 
-//numberr of items to load initially and each time the button is clicked
+//number of items to load initially and each time the button is clicked
 const itemsPerPage = 15;
 let currentPage = 1;
 
